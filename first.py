f25213ce5ae14659aa16df61c75513edc882e8f1
B&T <no@mail.sorry>
2019-07-06 03:17:21 +0000

    Initial

---
+++ first.py
@@ -0,0 +1,41 @@
+import os
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+token = os.environ['TOKEN']
+updater = Updater(token=token, use_context=True)
+
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    print(update.message.chat_id)
+    context.bot.send_message(chat_id=update.message.chat_id, 
+                             text="I'm a bot, please talk to me!")
+                             
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, 
+                             text=update.message.text)
+                             
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.message.chat_id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+updater.start_polling()
+updater.idle()
+
+
+
+
